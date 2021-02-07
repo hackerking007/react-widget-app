731a8994515750e8054ecad0f3bc7a4af560ca62
hackerking007 <ayjain71@gmail.com>
2021-02-07 23:04:44 +0530

    last commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    return <h1>Search</h1>
+
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const search = async() => {
+           const{data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+            setResults(data.query.search);
+        };
+        search();
+    }, [term]);
+
+
+
+    return (
+        <div>
+            <div className ="ui form">
+                <div className="field">
+                    <label>Search Bar</label>
+                    <input value={term} onChange = {(e) => setTerm(e.target.value)} className="input"></input>
+                </div>
+            </div>
+        </div>
+    );
 };
 export default Search;
