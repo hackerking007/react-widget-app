7bb7e8036a4df86ec560fd20481084d0e2f66f77
hackerking007 <ayjain71@gmail.com>
2021-02-08 23:00:41 +0530

    3nd commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,12 +19,31 @@ const Search = () => {
             });
             setResults(data.query.search);
         };
-        search();
+
+            if(term && !results.length)
+            {
+                search();
+            }
+            else{
+                const timeoutID = setTimeout(() => {
+                    if(term){
+                        search();
+                    }
+                }, 1000);
+        
+                return () => {
+                    clearTimeout(timeoutID)
+                };
+            }
+        
     }, [term]);
 
     const renderedResults = results.map((result) => {
         return(
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
                 <div className="content">
                     <div className="header">
                         {result.title}
